2ef5516bf2ffcc0f33fba9de63f462c26448ece3
denis <denisjimor@gmail.com>
2026-01-28 11:59:16 +0100

    update js y index

--- sw.js
+++ sw.js
@@ -1,9 +1,35 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "rutina-cache-v3";
+
+const FILES_TO_CACHE = [
+  "./",
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  clients.claim();
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => k !== CACHE_NAME && caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request).catch(() => caches.match("index.html"));
+    })
+  );
+});
